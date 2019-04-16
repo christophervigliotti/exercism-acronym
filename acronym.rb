5ed42319e7657cde0ea4c9c9daaa60a8701e1e66
Christopher Vigliotti <christopher@christophervigliotti.com>
2019-04-16 12:01:40 -0400

    revised / concise answer using map.  ITS A MAP!

--- acronym.rb
+++ acronym.rb
@@ -1,10 +1,16 @@
 class Acronym
   def self.abbreviate(text)
-    @acronym = ""
+    return text.split(/[\s,-]/).map{|i| i[0,1].upcase}.join
+    # split(/[\s,-]/) splits by space or dash
+    # map "maps" over an array
+    # {|i| i[0,1].upcase} iterates through each word, grabs the first letter and makes it uppercase...? and adds it to a new array?
+    # join converts the results back to a string
+  end
+  def self.abbreviate_version_1(text)
+    acronym = ""
     text.split(/[\s,',-]/).each { |word|
-        # version_1 @rna_nucleotides_string += get_rna_nucleotide(dna_nucleotide)
-        @acronym += word[0..0].upcase
+        acronym += word[0..0].upcase
     }
-    return @acronym
+    return acronym
   end
 end
